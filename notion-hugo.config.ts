810e377415cb269bec960b069b325a24858d4449
Viktor Ivanov <vantek1266@gmail.com>
2024-04-30 12:53:55 -0700

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://www.notion.so/sparkyourheartablaze/Notion-DoIt-d94d57cbdf0d47688bb6500baf880722?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
